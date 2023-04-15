3308116bc829f00f854bd70b2735975861b42c40
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2023-04-15 17:00:24 -0400

    set up done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+$('#main').css({
+  'font-size': '1.5rem',
+});
+
+let ani = $('#main').animate({
+  marginLeft: '65%',
+}, 7000).animate({
+  marginLeft: '0',
+}, 7000);
+
+setInterval(() => {
+  ani = $('#main').animate({
+    marginLeft: '65%',
+  }, 7000).animate({
+    marginLeft: '0',
+  }, 7000);
+
+  ani.promise().done(() => {
+    console.log('Animation complete.');
+  });
+}, 14000);
+
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
