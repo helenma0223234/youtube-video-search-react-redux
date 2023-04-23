d9fb1c5bfb01deb3c7f32e3dd39f872cd8332460
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2023-04-22 20:00:22 -0400

    vimeo API setup

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,14 +2,14 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import SearchBar from './search_bar';
 import youtubeSearch from '../services/youtube-api';
-// import vimeoSearch from '../services/vimeo-api';
+import vimeoSearch from '../services/vimeo-api';
 import VideoList from './video_list';
 import VideoDetail from './video_detail';
 
 function App(props) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelected] = useState(null);
-  const [resourceType, setResourceType] = useState('youtube');
+  const [resourceType, setResourceType] = useState('vimeo');
   const [searchText, setSearchText] = useState('pixar');
 
   const search = (text) => {
@@ -17,19 +17,22 @@ function App(props) {
 
     if (resourceType === 'youtube') {
       searchFunction = youtubeSearch;
+      searchFunction(text).then((result) => {
+        setVideos(result);
+        setSelected(result[0]);
+      });
+    } else if (resourceType === 'vimeo') {
+      searchFunction = vimeoSearch;
+      searchFunction(text).then((result) => {
+        setVideos(result);
+        setSelected(result[0]);
+      });
     }
-    // else if (resourceType === 'vimeo') {
-    //   searchFunction = vimeoSearch;
-    // }
-    searchFunction(text).then((result) => {
-      setVideos(result);
-      setSelected(result[0]);
-    });
   };
 
-  useEffect(() => {
-    search(searchText);
-  }, [resourceType]);
+  // useEffect(() => {
+  //   search(searchText);
+  // }, [resourceType, searchText]);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -39,7 +42,7 @@ function App(props) {
     return () => {
       clearTimeout(timeoutId);
     };
-  }, [searchText]);
+  }, [resourceType, searchText]);
 
   return (
     <div className="body-wrapper">
@@ -47,12 +50,11 @@ function App(props) {
         <h2>Video Quick Search</h2>
         <SearchBar onSearchChange={(text) => setSearchText(text)} />
       </div>
-      {/* <div>
+      <div>
         <button type="button" onClick={() => setResourceType('youtube')}>YouTube</button>
         <button type="button" onClick={() => setResourceType('vimeo')}>Vimeo</button>
-        <button type="button" onClick={() => setResourceType('giphy')}>GIPHY</button>
       </div>
-      <h1>{resourceType}</h1> */}
+      <h1>{resourceType}</h1>
       <div id="video-section">
         <VideoDetail video={selectedVideo} />
         <VideoList onVideoSelect={(selection) => setSelected(selection)} videos={videos} />
