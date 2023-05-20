88ae85edd504981fba45968d66f8001f351fc956
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2023-05-20 17:47:52 -0400

    merge redux + adjust

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
-=======
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
->>>>>>> bdaad6a231e0ddd8ee7b12da4e79a859d066e5bc
 import './style.scss';
 
 import App from './components/app';
 
-<<<<<<< HEAD
-const root = createRoot(document.getElementById('main'));
-root.render(<App />);
-=======
 import rootReducer from './reducers';
 
 // this creates the store with the reducers
@@ -26,4 +19,3 @@ root.render(
     <App />
   </Provider>,
 );
->>>>>>> bdaad6a231e0ddd8ee7b12da4e79a859d066e5bc
